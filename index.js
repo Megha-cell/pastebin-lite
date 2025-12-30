81f84325987c4300c525e4f8182bfce0d26c31ba
Megha-cell <meghakamlekar123@gmail.com>
2025-12-30 21:28:57 +0530

    Fix routing foFix vercel routing using route

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 import express from "express";
-import redis from "../redis.js";
+import redis from "./redis.js";
 import cors from "cors";
-import healthRoutes from "../routes/health.js";
-import pasteRoutes from "../routes/pasteRoutes.js"
-import { viewPaste } from "../controllers/viewPaste.js";
+import healthRoutes from "./routes/health.js";
+import pasteRoutes from "./routes/pasteRoutes.js"
+import { viewPaste } from "./controllers/viewPaste.js";
 const app = express();
 
 app.use(cors());
@@ -15,9 +15,9 @@ app.use(express.json());
 app.use("/api", healthRoutes);
 app.use("/api/pastes", pasteRoutes);
 app.get("/p/:id", viewPaste);
-export default app;
+// export default app;
 
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- redis.js
+++ redis.js
@@ -1,5 +1,12 @@
-import { Redis } from "@upstash/redis";
+import { createClient } from "redis";
 
-const redis = Redis.fromEnv();
+const redis = createClient({
+  url: process.env.REDIS_URL || "redis://127.0.0.1:6379",
+});
+
+redis.on("error", (err) => console.error("Redis error", err));
+redis.on("connect", () => console.log("Redis connected"));
+
+await redis.connect();
 
 export default redis;
