364ab9470c265255e121b266c3aebc7f28b873c1
Megha-cell <meghakamlekar123@gmail.com>
2025-12-30 16:07:59 +0530

    changes2

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ app.use(express.json());
 app.use("/api", healthRoutes);
 app.use("/api/pastes", pasteRoutes);
 app.get("/p/:id", viewPaste);
-export default app;
+//export default app;
 
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
