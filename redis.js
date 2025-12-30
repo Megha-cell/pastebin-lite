054389646b728ffdcb9e8376bb947d97b56fab7f
Megha-cell <meghakamlekar123@gmail.com>
2025-12-30 21:25:43 +0530

    Fix routing foFix vercel routing using routess

--- redis.js
+++ redis.js
@@ -1,12 +1,5 @@
-import { createClient } from "redis";
+import { Redis } from "@upstash/redis";
 
-const redis = createClient({
-  url: process.env.REDIS_URL || "redis://127.0.0.1:6379",
-});
-
-redis.on("error", (err) => console.error("Redis error", err));
-redis.on("connect", () => console.log("Redis connected"));
-
-await redis.connect();
+const redis = Redis.fromEnv();
 
 export default redis;
